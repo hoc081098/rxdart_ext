87a9533522de1c1ceeb2aa0cafb1d5687d1a21c6
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-12-23 01:50:10 +0700

    docs not_replay_value_connectable_stream.dart

--- lib/src/value/not_replay_value_connectable_stream.dart
+++ lib/src/value/not_replay_value_connectable_stream.dart
@@ -6,7 +6,10 @@ import 'package:rxdart/rxdart.dart'
 import 'not_replay_value_stream.dart';
 import 'value_subject.dart';
 
+/// A [ConnectableStream] that converts a single-subscription Stream into
+/// a broadcast [Stream], and provides synchronous access to the latest emitted value.
 ///
+/// See [NotReplayValueStream].
 class NotReplayValueConnectableStream<T> extends ConnectableStream<T>
     implements NotReplayValueStream<T> {
   final ValueSubject<T> _subject;
@@ -16,11 +19,14 @@ class NotReplayValueConnectableStream<T> extends ConnectableStream<T>
   NotReplayValueConnectableStream._(this._source, this._subject)
       : super(_subject);
 
-  ///
+  /// Constructs a [Stream] which only begins emitting events when
+  /// the [connect] method is called, this [Stream] acts like a [ValueSubject].
   factory NotReplayValueConnectableStream(Stream<T> source, T seedValue,
           {bool sync = true}) =>
       NotReplayValueConnectableStream._(
-          source, ValueSubject(seedValue, sync: sync));
+        source,
+        ValueSubject(seedValue, sync: sync),
+      );
 
   void _checkUsed() {
     if (_used) {
@@ -86,12 +92,55 @@ class NotReplayValueConnectableStream<T> extends ConnectableStream<T>
 
 ///
 extension ValueConnectableNotReplayStreamExtensions<T> on Stream<T> {
+  /// Convert the current Stream into a [ConnectableStream] that can be listened
+  /// to multiple times. It will not begin emitting items from the original
+  /// Stream until the `connect` method is invoked.
   ///
+  /// This is useful for converting a single-subscription stream into a
+  /// broadcast Stream.
+  ///
+  /// ### Example
+  ///
+  /// ```
+  /// final source = Stream.fromIterable([1, 2, 3]);
+  /// final connectable = source.publish();
+  ///
+  /// // Does not print anything at first
+  /// connectable.listen(print);
+  ///
+  /// // Start listening to the source Stream. Will cause the previous
+  /// // line to start printing 1, 2, 3
+  /// final subscription = connectable.connect();
+  ///
+  /// // Stop emitting items from the source stream and close the underlying
+  /// // Subject
+  /// subscription.cancel();
+  /// ```
   NotReplayValueConnectableStream<T> publishValueNotReplay(T seedValue,
           {bool sync = true}) =>
       NotReplayValueConnectableStream(this, seedValue, sync: sync);
 
+  /// Convert the current Stream into a new Stream that can be listened
+  /// to multiple times. It will automatically begin emitting items when first
+  /// listened to, and shut down when no listeners remain.
   ///
+  /// This is useful for converting a single-subscription stream into a
+  /// broadcast Stream.
+  ///
+  /// ### Example
+  ///
+  /// ```
+  /// // Convert a single-subscription fromIterable stream into a broadcast
+  /// // stream
+  /// final stream =  Stream.fromIterable([1, 2, 3]).share();
+  ///
+  /// // Start listening to the source Stream. Will start printing 1, 2, 3
+  /// final subscription = stream.listen(print);
+  ///
+  /// // Stop emitting items from the source stream and close the underlying
+  /// // PublishSubject
+  /// subscription.cancel();
+  /// ```
   NotReplayValueStream<T> shareValueNotReplay(T seedValue) =>
       publishValueNotReplay(seedValue).refCount();
 }
