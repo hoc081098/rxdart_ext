3963933a2f2b2d5d24c0dd6add48515141c5c27f
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-01-30 17:11:42 +0700

    :white_check_mark: Adding tests.

--- test/rxdart_ext_test.dart
+++ test/rxdart_ext_test.dart
@@ -1,12 +1,15 @@
+import 'value/value_controller_test.dart' as value_controller_test;
+import 'value/value_subject_test.dart' as value_subject_test;
+
 import 'debug_test.dart' as debug_test;
 import 'distinct_by_test.dart' as distinct_by_test;
 import 'distinct_unique_by_test.dart' as distinct_unique_by_test;
 import 'ignore_test.dart' as ignore_test;
 import 'map_not_null_test.dart' as map_not_null_test;
 import 'to_single_subscription_test.dart' as to_single_subscription_test;
-import 'value/value_subject_test.dart' as value_subject_test;
 
 void main() {
+  value_controller_test.main();
   value_subject_test.main();
   debug_test.main();
   distinct_by_test.main();
