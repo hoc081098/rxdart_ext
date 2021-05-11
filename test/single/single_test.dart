8491acec55380127882b75a502efe518bbe2d89c
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-05-11 22:58:39 +0700

    reworks tests

--- test/single/single_test.dart
+++ test/single/single_test.dart
@@ -2,15 +2,6 @@ import 'package:dart_either/dart_either.dart';
 import 'package:rxdart_ext/rxdart_ext.dart';
 import 'package:test/test.dart';
 
-class Repo {
-  Single<String> getSomething() => Single.fromCallable(
-        () => Future.delayed(
-          const Duration(seconds: 2),
-          () => 'something',
-        ),
-      );
-}
-
 Future<void> singleRule<T>(Stream<T> single, Either<Object, T> e) {
   return expectLater(
     single,
@@ -24,87 +15,58 @@ Future<void> singleRule<T>(Stream<T> single, Either<Object, T> e) {
 final _left = Either<Object, Never>.left(isA<Exception>());
 
 void main() {
-  test('singleOrError', () async {
-    // Single.value
-    await singleRule(
-      Single.value(1),
-      Either.right(1),
-    );
+  group('Single', () {
+    test('singleRule', () async {
+      // Single.value
+      await singleRule(
+        Single.value(1),
+        Either.right(1),
+      );
 
-    // Single.error
-    await singleRule(
-      Single<int>.error(Exception()),
-      _left,
-    );
+      // Single.error
+      await singleRule(
+        Single<int>.error(Exception()),
+        _left,
+      );
 
-    // Single.map.success
-    await singleRule(
-      Single.value(1).map((event) => event.toString()),
-      Either.right('1'),
-    );
-    // Single.map.failure
-    await singleRule(
-      Single.value(1).map((event) => throw Exception()),
-      _left,
-    );
+      // Single.map.success
+      await singleRule(
+        Single.value(1).map((event) => event.toString()),
+        Either.right('1'),
+      );
+      // Single.map.failure
+      await singleRule(
+        Single.value(1).map((event) => throw Exception()),
+        _left,
+      );
 
-    // Single.asyncMap.sync.success
-    await singleRule(
-      Single.value(1).asyncMap((event) => event.toString()),
-      Either.right('1'),
-    );
-    // Single.asyncMap.sync.failure
-    await singleRule(
-      Single.value(1).asyncMap((event) => throw Exception()),
-      _left,
-    );
+      // Single.asyncMap.sync.success
+      await singleRule(
+        Single.value(1).asyncMap((event) => event.toString()),
+        Either.right('1'),
+      );
+      // Single.asyncMap.sync.failure
+      await singleRule(
+        Single.value(1).asyncMap((event) => throw Exception()),
+        _left,
+      );
 
-    // Single.asyncMap.async.success
-    await singleRule(
-      Single.value(1).asyncMap((event) async {
-        await Future<void>.delayed(const Duration(milliseconds: 100));
-        return event.toString();
-      }),
-      Either.right('1'),
-    );
-    // Single.asyncMap.async.failure
-    await singleRule(
-      Single.value(1).asyncMap((event) async {
-        await Future<void>.delayed(const Duration(milliseconds: 100));
-        throw Exception();
-      }),
-      _left,
-    );
-
-    return;
-
-    Single.value(1).debug().listen((event) {});
-    Single<int>.error(1).debug().listen((event) {});
-
-    Stream<int>.error(1)
-        .concatWith([Stream.value(1)])
-        .singleOrError()
-        .listen((value) {});
-    //
-    // final s = PublishSubject<int>();
-    //
-    // s.singleOrError().debug(identifier: '1 >>>>').collect();
-    // s.add(1);
-    // s.singleOrError().debug(identifier: '2 >>>>').collect();
-    // s.add(2);
-    // s.close();
-    //
-    // Stream<void>.fromIterable([1, 2, 3])
-    //     .singleOrError()
-    //     .onErrorResumeNext(Stream.empty())
-    //     .listen(print);
-    //
-    // Repo().getSomething().listen(print);
-    // Stream.value(1)
-    //     .exhaustMap(
-    //         (value) => Repo().getSomething().map((event) => '$event $value'))
-    //     .listen(print);
-
-    await Future<void>.delayed(const Duration(seconds: 5));
+      // Single.asyncMap.async.success
+      await singleRule(
+        Single.value(1).asyncMap((event) async {
+          await Future<void>.delayed(const Duration(milliseconds: 100));
+          return event.toString();
+        }),
+        Either.right('1'),
+      );
+      // Single.asyncMap.async.failure
+      await singleRule(
+        Single.value(1).asyncMap((event) async {
+          await Future<void>.delayed(const Duration(milliseconds: 100));
+          throw Exception();
+        }),
+        _left,
+      );
+    });
   });
 }
