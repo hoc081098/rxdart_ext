cd7b079a56bd383bdea1f5eb22c0ead5d767c156
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-05-12 20:14:25 +0700

    Update not_replay_value_stream.dart
    Fix typo

--- lib/src/value/not_replay_value_stream.dart
+++ lib/src/value/not_replay_value_stream.dart
@@ -1,5 +1,5 @@
 import 'package:rxdart/rxdart.dart' show ValueStream;
 
-/// An [Stream] that provides synchronous access to the last emitted item,
+/// A [Stream] that provides synchronous access to the last emitted item,
 /// but not replay the latest value.
 abstract class NotReplayValueStream<T> extends ValueStream<T> {}
