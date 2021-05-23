72c14bf16eae66c491f7b1060c888f28c638dcae
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-05-23 22:26:55 +0700

    update flatMap and switchMap

--- lib/src/single/flat_map.dart
+++ lib/src/single/flat_map.dart
@@ -1,10 +1,13 @@
 import 'package:rxdart/rxdart.dart';
 
+import 'exhaust_map.dart';
 import 'single.dart';
 
 /// Provides [flatMapSingle] extension for [Single].
 extension FlatMapSingleExtension<T> on Single<T> {
   /// Likes [flatMap], but returns a [Single].
+  ///
+  /// This function is an alias to [exhaustMapSingle] operator.
   Single<R> flatMapSingle<R>(Single<R> Function(T) transform) =>
-      Single.safe(flatMap(transform));
+      exhaustMapSingle(transform);
 }
--- lib/src/single/switch_map.dart
+++ lib/src/single/switch_map.dart
@@ -1,10 +1,13 @@
 import 'package:rxdart/rxdart.dart';
 
+import 'exhaust_map.dart';
 import 'single.dart';
 
 /// Provides [switchMapSingle] extension for [Single].
 extension SwitchMapSingleExtension<T> on Single<T> {
   /// Likes [switchMap], but returns a [Single].
+  ///
+  /// This function is an alias to [exhaustMapSingle] operator.
   Single<R> switchMapSingle<R>(Single<R> Function(T) transform) =>
-      Single.safe(switchMap(transform));
+      Single.safe(exhaustMapSingle(transform));
 }
