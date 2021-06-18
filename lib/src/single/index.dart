9584e83cd337e98ba2bc9b10b4a39c3bb7ef0ba8
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-06-18 16:13:45 +0700

    add

--- lib/src/single/index.dart
+++ lib/src/single/index.dart
@@ -5,6 +5,7 @@ export 'delay.dart';
 export 'do.dart';
 export 'exhaust_map.dart';
 export 'flat_map.dart';
+export 'map_to.dart';
 export 'on_error_resume.dart';
 export 'single.dart';
 export 'single_or_error.dart';
---
+++ lib/src/single/map_to.dart
@@ -0,0 +1,15 @@
+import '../../rxdart_ext.dart';
+import 'single.dart';
+
+/// Extends the Single class with the ability to convert the item to the same value.
+extension MapToSingleExtension<T> on Single<T> {
+  /// Emits the given constant value on the output Single when the source
+  /// Single emits a value.
+  ///
+  /// ### Example
+  ///
+  ///     Single.value(1)
+  ///       .mapTo(true)
+  ///       .listen(print); // prints true
+  Single<R> mapTo<R>(R value) => Single.safe(MapToExtension(this).mapTo(value));
+}
