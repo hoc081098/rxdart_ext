6d018bbec6bf64803caadda11af23f03922f9402
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-08-23 22:33:36 +0700

    forward to StateStream.defaultEquals

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -2,10 +2,13 @@ import 'package:meta/meta.dart';
 import 'package:path/path.dart' as path;
 import 'package:stack_trace/stack_trace.dart';
 
+import '../state_stream.dart';
+
 // ignore_for_file: public_member_api_docs
 
 /// Default equals comparison, uses [Object.==].
-bool defaultEquals(Object? lhs, Object? rhs) => lhs == rhs;
+bool Function(Object? lhs, Object? rhs) get defaultEquals =>
+    StateStream.defaultEquals;
 
 /// Default hash code. returns [Object.hashCode].
 int defaultHashCode(Object? o) => o.hashCode;
