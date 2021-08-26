2f6de4cdd196a71e5dc11e5b93be3d426c5bf83d
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-08-27 00:50:55 +0700

    update(example) [skip ci]

--- example/lib/rxdart_ext_example.dart
+++ example/lib/rxdart_ext_example.dart
@@ -1,5 +1,16 @@
-import 'package:rxdart_ext/operators.dart';
+import 'package:rxdart_ext/rxdart_ext.dart';
 
 void main() {
   Stream.fromIterable([1, 2, 3, 4]).debug().collect();
+
+  final state$ = StateSubject(1);
+  state$.debug(identifier: '<<State>>').collect();
+  print(state$.value);
+
+  RxSingles.zip2(
+    Single.value(1).delay(const Duration(milliseconds: 100)),
+    Single.fromCallable(
+        () => Future.delayed(const Duration(milliseconds: 200), () => 2)),
+    (int p0, int p1) => p0 + p1,
+  ).doOnData(state$.add).listen((_) => print(state$.value));
 }
