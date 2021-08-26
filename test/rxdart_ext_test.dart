5c2521e893975c4e2868b73f2ab999300fb62366
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-08-27 00:41:41 +0700

    update(tests): restructure (#31)

--- test/rxdart_ext_test.dart
+++ test/rxdart_ext_test.dart
@@ -1,17 +1,19 @@
-import 'debug_test.dart' as debug_test;
-import 'distinct_by_test.dart' as distinct_by_test;
-import 'distinct_unique_by_test.dart' as distinct_unique_by_test;
-import 'do_on_test.dart' as do_on_test;
-import 'ignore_test.dart' as ignore_test;
-import 'map_not_null_test.dart' as map_not_null_test;
-import 'to_single_subscription_test.dart' as to_single_subscription_test;
-import 'void_test.dart' as void_test;
-import 'where_not_null_test.dart' as where_not_null_test;
+import 'operators/debug_test.dart' as debug_test;
+import 'operators/distinct_by_test.dart' as distinct_by_test;
+import 'operators/distinct_unique_by_test.dart' as distinct_unique_by_test;
+import 'operators/do_on_test.dart' as do_on_test;
+import 'operators/ignore_test.dart' as ignore_test;
+import 'operators/map_not_null_test.dart' as map_not_null_test;
+import 'operators/to_single_subscription_test.dart'
+    as to_single_subscription_test;
+import 'operators/void_test.dart' as void_test;
+import 'operators/where_not_null_test.dart' as where_not_null_test;
 
-import 'value/not_replay_value_connectable_stream_tests.dart'
+import 'not_replay_value_stream/not_replay_value_connectable_stream_tests.dart'
     as not_replay_value_connectable_stream_tests;
-import 'value/value_controller_test.dart' as value_controller_test;
-import 'value/value_subject_test.dart' as value_subject_test;
+import 'not_replay_value_stream/value_controller_test.dart'
+    as value_controller_test;
+import 'not_replay_value_stream/value_subject_test.dart' as value_subject_test;
 
 import 'single/as_single_test.dart' as as_single_test;
 import 'single/as_void_test.dart' as as_void_test;
@@ -27,7 +29,7 @@ import 'single/singles_test.dart' as singles_test;
 import 'single/switch_flat_exhaust_map_test.dart'
     as switch_flat_exhaust_map_test;
 
-import 'controller/add_null_test.dart' as add_null_test;
+import 'utils/add_null_test.dart' as add_null_test;
 
 import 'state_stream/to_state_stream_test.dart' as to_state_stream_test;
 import 'state_stream/state_subject_test.dart' as state_subject_test;
