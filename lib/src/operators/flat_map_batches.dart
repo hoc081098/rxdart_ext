620ab6458af9330e018d51ba804a374eda119465
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-09-04 21:49:57 +0700

    update flatMapBatches

--- lib/src/operators/flat_map_batches.dart
+++ lib/src/operators/flat_map_batches.dart
@@ -35,48 +35,9 @@ extension FlatMapBatchesStreamExtension<T> on Stream<T> {
 
     return bufferCount(maxConcurrent)
         .asyncExpand(convert)
-        .scan<List<R>>((acc, value, _) => [...acc, ...value], [])
+        .scan<List<R>>((acc, value, _) => acc..addAll(value), [])
         .takeLast(1)
+        .map((value) => List<R>.unmodifiable(value))
         .takeFirstDataOrFirstErrorAndClose();
   }
 }
-//
-// Single<int> api(int count) {
-//   return Single.fromCallable(() async {
-//     print('Call.. $count');
-//     await Future<void>.delayed(const Duration(seconds: 1));
-//     return count;
-//   });
-// }
-//
-// void main() async {
-//   final ids = List.generate(10, (index) => index);
-//   Stream.fromIterable(ids)
-//       .flatMapBatchesSingle(api, 3)
-//       .listen(print, onError: print);
-//
-//   await Future<void>.delayed(const Duration(seconds: 2));
-//
-//   return;
-//
-//   await Rx.range(0, 10)
-//       .flatMapBatches(
-//         (p0) async* {
-//           print('Start $p0, delay ${p0 % 3}');
-//           await Future<void>.delayed(Duration(seconds: p0 % 3));
-//           print('Done $p0');
-//           yield p0;
-//         },
-//         3,
-//       )
-//       .scan<List<int>>((acc, value, index) => [...acc, ...value], <int>[])
-//       .takeLast(1)
-//       .forEach((v) => print('>>>>>>>>>>>>>>>>>>> $v'));
-//
-//   await Stream.fromIterable([
-//     Stream.fromIterable([1, 2]),
-//     Stream.fromIterable([1, 2]),
-//     Stream.value(3),
-//     Stream.value(4),
-//   ]).flatMapBatches((p0) => p0, 2).forEach(print);
-// }
