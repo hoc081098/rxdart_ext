de354f8ab29b8bfd16847d9278a49bd7905d80be
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-09-04 21:46:43 +0700

    fix single/do

--- lib/src/utils/identity.dart
+++ lib/src/utils/identity.dart
@@ -1,4 +1,2 @@
-// ignore_for_file: prefer_function_declarations_over_variables
-
 /// A function that always returns the same value that was used as its argument.
-final T Function<T>(T) identity = <T>(T i) => i;
+T identity<T>(T i) => i;
