75f95ac3b46148499a7b61bcc5146afa83108f7d
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-09-24 16:53:36 +0700

    add StateStream.asBroadcastStateStream (#48)
    * asBroadcastStream
    
    * as_broadcast_test
    
    * fix
    
    * fix
    
    * fix

---
+++ lib/src/state_stream/as_broadcast.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+
+import 'state_stream.dart';
+import 'state_stream_mixin.dart';
+
+/// Convert a [StateStream] to a broadcast [StateStream].
+extension BroadcastStateStreamExtensions<T> on StateStream<T> {
+  /// Convert the this [StateStream] into a new [StateStream] that can
+  /// be listened to multiple times, providing an initial value.
+  /// It will automatically begin emitting items when first listened to,
+  /// and shut down when no listeners remain.
+  ///
+  /// This is useful for converting a single-subscription stream into a
+  /// broadcast Stream. It's also useful for providing sync access to the latest
+  /// emitted value.
+  @internal
+  StateStream<T> asBroadcastStateStream() => _AsBroadcastStateStream(this);
+}
+
+class _AsBroadcastStateStream<T> extends StreamView<T>
+    with StateStreamMixin<T>
+    implements StateStream<T> {
+  final StateStream<T> source;
+
+  _AsBroadcastStateStream(this.source)
+      : super(source.asBroadcastStream(onCancel: (s) => s.cancel()));
+
+  @override
+  bool Function(T p1, T p2) get equals => source.equals;
+
+  @override
+  T get value => source.value;
+}
---
+++ test/state_stream/as_broadcast_test.dart
@@ -0,0 +1,54 @@
+import 'package:rxdart_ext/rxdart_ext.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('StateStream.asBroadcastStream', () async {
+    final elements = [0, 1, 1, 2, 3, 4, 4];
+    final expected = <Object>[1, 2, 3, 4, emitsDone];
+
+    Future<void> _test(StateStream<int> stream, StateStream<int> source) async {
+      expect(identical(stream.equals, source.equals), true);
+      expect(stream.errorOrNull, null);
+      expect(stream.stackTrace, null);
+      expect(stream.value, 0);
+      expect(stream.valueOrNull, 0);
+      expect(stream.hasValue, true);
+      expect(stream.hasError, false);
+      expect(stream.isBroadcast, true);
+
+      expect(stream, emitsInOrder(expected));
+      expect(stream, emitsInOrder(expected));
+      await expectLater(stream, emitsInOrder(expected));
+
+      await pumpEventQueue();
+      expect(stream, emitsDone);
+    }
+
+    {
+      final source = Stream.fromIterable(elements).toStateStream(0);
+      final stream = source.asBroadcastStateStream();
+      await _test(stream, source);
+    }
+
+    {
+      final source =
+          Stream.fromIterable(elements).asBroadcastStream().toStateStream(0);
+      final stream = source.asBroadcastStateStream();
+      await _test(stream, source);
+    }
+
+    {
+      final source = Stream.fromIterable(elements)
+          .toStateStream(0)
+          .asBroadcastStateStream();
+      final stream = source.asBroadcastStateStream();
+      await _test(stream, source);
+    }
+
+    {
+      final source = Stream.fromIterable(elements).shareState(0);
+      final stream = source.asBroadcastStateStream();
+      await _test(stream, source);
+    }
+  });
+}
