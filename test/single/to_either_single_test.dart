a35914709a03f9868de9324ed328dd253302a21a
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2022-08-25 17:18:09 +0700

    Single.toEitherSingle test

---
+++ test/single/to_either_single_test.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_function_declarations_over_variables
+
+import 'package:dart_either/dart_either.dart';
+import 'package:rxdart_ext/rxdart_ext.dart';
+import 'package:test/scaffolding.dart';
+import 'package:test/test.dart';
+
+import 'utils.dart';
+
+void main() {
+  group('Single.toEitherSingle', () {
+    test('.success', () async {
+      final build = () => Single.value(1).toEitherSingle((e, s) => e);
+      await singleRule(build(), 1.right<Object>().right());
+      broadcastRule(build(), false);
+      await cancelRule(build());
+    });
+
+    test('.failure', () async {
+      final build =
+          () => Single<int>.error(Exception()).toEitherSingle((e, s) => e);
+      await singleRule(
+        build(),
+        isA<Left<Object, int>>()
+            .having((l) => l.value, 'Left.value', isException)
+            .right(),
+      );
+      broadcastRule(build(), false);
+      await cancelRule(build());
+    });
+  });
+}
