c2822bc9e9757e72152ca5184378605ece322933
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2022-11-15 13:47:32 +0700

    feat(RxSingles.using): add `RxSingles.using` (#77)
    * feat(RxSingles.using): add `RxSingles.using`
    
    * feat(RxSingles.using): add `RxSingles.using`

--- test/single/rx_singles_test.dart
+++ test/single/rx_singles_test.dart
@@ -1,10 +1,25 @@
 // ignore_for_file: prefer_function_declarations_over_variables
 
+import 'dart:async';
+
 import 'package:dart_either/dart_either.dart';
 import 'package:rxdart_ext/rxdart_ext.dart';
 import 'package:test/test.dart';
 
 import 'utils.dart';
+
+class TestResource {
+  var _isClosed = false;
+
+  void close() {
+    if (_isClosed) {
+      throw StateError('Already closed');
+    }
+    _isClosed = true;
+  }
+
+  bool get isClosed => _isClosed;
+}
 
 void main() {
   group('RxSingles.zip2', () {
@@ -108,6 +123,111 @@ void main() {
       await singleRule(build(), exceptionLeft);
       broadcastRule(build(), false);
       await cancelRule(build());
+    });
+  });
+
+  group('RxSingles.using', () {
+    test('resourceFactory throws', () async {
+      final build = () => RxSingles.using<int, TestResource>(
+            () => throw Exception(),
+            (r) => fail('should not be called'),
+            (r) => fail('should not be called'),
+          );
+
+      await singleRule(build(), exceptionLeft);
+      broadcastRule(build(), false);
+      await cancelRule(build());
+    });
+
+    test('success', () async {
+      TestResource? resource;
+
+      void clear() {
+        resource = null;
+      }
+
+      final build = () {
+        if (resource != null) {
+          throw StateError('Resource already created');
+        }
+        return RxSingles.using<TestResource, TestResource>(
+          () => resource = TestResource(),
+          (r) => Single.value(r),
+          (r) => r.close(),
+        );
+      };
+
+      await singleRule(build(), isA<TestResource>().right());
+      expect(resource!.isClosed, true);
+
+      clear();
+      broadcastRule(build(), false);
+
+      clear();
+      await cancelRule(build());
+      expect(resource == null || resource!.isClosed, true);
+
+      clear();
+      await cancelRule(build(), Duration.zero);
+      expect(resource == null || resource!.isClosed, true);
+    });
+
+    test('failure', () async {
+      TestResource? resource;
+
+      void clear() {
+        resource = null;
+      }
+
+      final build = () {
+        if (resource != null) {
+          throw StateError('Resource already created');
+        }
+        return RxSingles.using<TestResource, TestResource>(
+          () => resource = TestResource(),
+          (r) => Single.error(Exception()),
+          (r) => r.close(),
+        );
+      };
+
+      await singleRule(build(), exceptionLeft);
+      expect(resource!.isClosed, true);
+
+      clear();
+      broadcastRule(build(), false);
+
+      clear();
+      await cancelRule(build());
+      expect(resource == null || resource!.isClosed, true);
+
+      clear();
+      await cancelRule(build(), Duration.zero);
+      expect(resource == null || resource!.isClosed, true);
+    });
+
+    test('disposer throws', () async {
+      final build = () => RxSingles.using<TestResource, TestResource>(
+            () => TestResource(),
+            (r) => Single.value(r),
+            (r) => throw Exception('Disposer'),
+          );
+
+      final onError = (Object error, StackTrace stack) {
+        expect(error, isA<Exception>());
+        expect(error.toString(), 'Exception: Disposer');
+      };
+      await runZonedGuarded(
+        () => singleRule(build(), isA<TestResource>().right()),
+        onError,
+      );
+      runZonedGuarded(
+        () => broadcastRule(build(), false),
+        onError,
+      );
+      await runZonedGuarded(
+        () => cancelRule(build()),
+        onError,
+      );
     });
   });
 }
