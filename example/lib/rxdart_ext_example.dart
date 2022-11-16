c0b560d6527f016cd15634dc7b559e4c476e6d46
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2022-11-16 23:46:09 +0700

    refactor(example)

--- example/lib/rxdart_ext_example.dart
+++ example/lib/rxdart_ext_example.dart
@@ -1,57 +1,116 @@
 import 'package:rxdart_ext/rxdart_ext.dart';
 
 void main() async {
+  await runDebugAndCollectOperatorExample();
+  printSeparator();
+
+  await runFlatMapBatchesSingleExample();
+  printSeparator();
+
+  await runRxSinglesZip2Example();
+  printSeparator();
+
+  await runRxSinglesUsingExample();
+  printSeparator();
+
+  await runDisposableMixinExample();
+}
+
+void printSeparator() {
+  print('');
+  print('-' * 80);
+  print('');
+}
+
+Future<void> runDebugAndCollectOperatorExample() async {
   Stream.fromIterable([1, 2, 3, 4]).debug(identifier: '[DEBUG]').collect();
   await delay(0);
+}
 
+Future<void> runFlatMapBatchesSingleExample() async {
   Stream.fromIterable([1, 2, 3, 4])
       .flatMapBatchesSingle(
           (v) => Single.timer(v, const Duration(milliseconds: 100)), 2)
       .debug(identifier: '[flatMapBatchesSingle]')
       .collect();
   await delay(500);
+}
 
-  Stream.fromIterable(['1', '.', '3', '5', '@'])
-      .mapNotNull(int.tryParse)
-      .debug(identifier: '[mapNotNull]')
-      .collect();
-  await delay(0);
-
+Future<void> runRxSinglesZip2Example() async {
   final state$ = StateSubject(1);
   state$.debug(identifier: '<<State>>').collect();
-  print('<<State>> ${state$.value}');
+  print('<<State>> [1] state\$.value=${state$.value}');
 
   await RxSingles.zip2(
     Single.value(1).delay(const Duration(milliseconds: 100)),
     Single.fromCallable(() => delay(200).then((_) => 2)),
-    (int p0, int p1) => p0 + p1,
-  ).doOnData(state$.add).forEach((_) => print('<<State>> ${state$.value}'));
+    (int v1, int v2) => v1 + v2,
+  )
+      .doOnData(state$.add)
+      .forEach((_) => print('<<State>> [2] state\$.value=${state$.value}'));
+}
 
-  await runDisposableMixinExample();
+class MyResource {
+  var _disposed = false;
+
+  MyResource() {
+    print('[using] MyResource()');
+  }
+
+  void dispose() {
+    if (_disposed) return;
+    _disposed = true;
+    print('[using] MyResource.dispose()');
+  }
+
+  Future<int> work() async {
+    await delay(200);
+    if (_disposed) {
+      print('[using] MyResource.work() - disposed');
+      throw Exception('MyResource is disposed');
+    }
+    return 42;
+  }
+}
+
+Future<void> runRxSinglesUsingExample() async {
+  final subscription = RxSingles.using<int, MyResource>(
+    () => MyResource(),
+    (r) => r.work().asSingle(),
+    (r) => r.dispose(),
+  ).debug(identifier: '[using]').collect();
+
+  await delay(100);
+  await subscription.cancel();
+  await delay(200);
 }
 
 Future<void> runDisposableMixinExample() async {
   final dateTimeStream = BehaviorSubject<DateTime>.seeded(
     DateTime.now().toUtc(),
   );
-  print('Disposable example created');
+
+  print('[DisposableMixin] Disposable example created');
   final disposableExample = DisposableExample(
     dateTimeStream: dateTimeStream,
   );
-  print('Periodic stream created');
+
+  print('[DisposableMixin] Periodic stream created');
   final periodicStreamSub = Stream<void>.periodic(
     const Duration(milliseconds: 100),
   ).listen((_) {
     final value = DateTime.now().toUtc();
-    print('Periodic stream: $value');
+    print('[DisposableMixin] Periodic stream emits: $value');
     dateTimeStream.add(value);
   });
+
   await delay(500);
   disposableExample.dispose();
-  print('Disposable example disposed');
+  print('[DisposableMixin] Disposable example disposed');
+
   await delay(500);
   await periodicStreamSub.cancel();
-  print('Periodic stream disposed');
+  print('[DisposableMixin] Periodic stream disposed');
 }
 
 class DisposableExample with DisposableMixin {
@@ -59,7 +118,8 @@ class DisposableExample with DisposableMixin {
     required Stream<DateTime> dateTimeStream,
   }) {
     dateTimeStream.takeUntil(dispose$).listen(
-          (value) => print('Disposable example: $value'),
+          (value) =>
+              print('[DisposableMixin] Disposable example receives: $value'),
         );
   }
 }
